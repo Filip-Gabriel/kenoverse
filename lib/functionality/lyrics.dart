93bf5ae3200a5f587f9b65805219354cc0d75366
Filip-Gabriel <gabrielfilip2048@gmail.com>
2026-04-21 10:50:31 +0300

    Initial commit

--- lib/functionality/lyrics.dart
+++ lib/functionality/lyrics.dart
@@ -1,6 +1,13 @@
-class song{
-  string title;
-  string lyrics;
-  image thumbnail;
-  newSong(){}
+import 'dart:ui';
+
+class Song {
+  late String lyrics;
+  late Image thumbnail;
+
+  void addLyrics(String lyrics) {
+    this.lyrics = lyrics;
+  }
+  void addThumbnail(Image thumbnail) {
+    this.thumbnail = thumbnail;
+  }
 }
